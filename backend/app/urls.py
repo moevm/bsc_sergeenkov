7e0e4770a8629f1b9d766acc55676a14d181ed0e
Mishelles <sergeenkov.michael@gmail.com>
2020-03-02 12:38:19 +0300

    Созданы модели для представления комментариев и авторов комментариев

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+admin.site.site_header = "Система автоматизации поддержки студентов МООК"
+admin.site.site_title = "Система автоматизации поддержки студентов МООК"
+admin.site.index_title = "Система автоматизации поддержки студентов МООК"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
